15ab37af2ae1853c5390e87c24fdeb6a163ed8c3
justin_james <jjames11799@gmail.com>
2020-06-11 13:43:19 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,32 @@
 class Deck
 
+    attr_accessor :cards 
+
+    def initialize()
+        @cards = []
+        suits = ["Hearts", "Clubs", "Diamonds", "Spades"] 
+        rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suits.each do |suit|
+            rank.size.times do |i|
+                @cards << Card.new(suit, rank[i])
+            end
+        end
+    end
+    
+    def choose_card
+        removed = cards.sample
+        cards.delete_if {|card| card == removed}
+        removed     
+    end
+
 end
 
 class Card
 
+    attr_accessor :rank, :suit 
+
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit 
+    end
 end
